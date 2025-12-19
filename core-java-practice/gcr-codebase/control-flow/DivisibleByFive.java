db4b9e820e18c6cd270cf381f73b0ea319b1cf1a
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-12-19 14:58:29 +0530

    Vishwajeet singh .add control flow directory initialized with check wheather the number is divisible by 5 or not

---
+++ core-java-practice/gcr-codebase/control-flow/DivisibleByFive.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class DivisibleByFive {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = in.nextInt();
+        if (number % 5 == 0) {
+            System.out.println(number + " is divisible by 5.");
+        } else {
+            System.out.println(number + " is not divisible by 5.");
+        }
+    }
+}
