55dc9dbc2cd7c28ce8bac73de842de93b222e671
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-12-19 15:31:04 +0530

    Vishwajeet singh .add find sum until user inputs zero or negative number

--- core-java-practice/gcr-codebase/control-flow/SumUntilZeroOrNegative.java
+++ core-java-practice/gcr-codebase/control-flow/SumUntilZeroOrNegative.java
@@ -1,12 +1,12 @@
 import java.util.Scanner;
-public class SumUntilZero{
+public class SumUntilZeroOrNegative{
     public static void main(String[] args){
         Scanner in=new Scanner(System.in);
         double total=0.0;
         while(true){
             System.out.print("Enter a number(0 or negative to stop):");
             double number=in.nextDouble();
-            if(number==0){
+            if(number<=0){
                 break;
             }
             total+=number;
