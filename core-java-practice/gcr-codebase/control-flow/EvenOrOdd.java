1315b8edce7b89479e0b27400edd25343c680961
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-12-19 15:46:44 +0530

    Vishwajeet singh Add. Even or odd between 1 to n

---
+++ core-java-practice/gcr-codebase/control-flow/EvenOrOdd.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = in.nextInt();
+        for(int i = 1 ; i < n ; i++){
+            if (i % 2 == 0) {
+                System.out.println(number + " is even.");
+            } else {
+                System.out.println(number + " is odd.");
+            }
+        }
+    }
+}
