1d19839243e4d48ab82018b60d96ec90565118c5
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-12-20 10:57:23 +0530

    [Vishwajeet Singh] Add . calculate bmi of person code added

---
+++ core-java-practice/gcr-codebase/control-flow/PersonBMI.java
@@ -0,0 +1,23 @@
+// Program to calculate BMI and determine weight status
+import java.util.*;
+public class PersonBMI {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the weight of the person (in kg): ");
+        double weight = in.nextDouble();
+        System.out.print("Enter the height of the person (in cm): ");
+        double height = in.nextDouble();
+        double heightInMeter = height / 100;
+        double bmi = weight / (heightInMeter * heightInMeter);
+        System.out.println("BMI of the person is: " + bmi);
+        if (bmi <= 18.4) {
+            System.out.println("Underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.println("Normal");
+        } else if (bmi >= 25 && bmi < 40) {
+            System.out.println("Overweight");
+        } else {
+            System.out.println("Obese");
+        }
+    }
+}
