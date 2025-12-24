356a3b04de708531343d4a6ba6f363915c4bca2d
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-12-24 11:42:49 +0530

    [Vishwajeet Singh] Add . mobile phones detail code added

---
+++ OOPs/gcr-codebase/class-and-object/MobilePhone/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        MobilePhone mobile1 = new MobilePhone("Apple", "iPhone 13", 999.99);
+        MobilePhone mobile2 = new MobilePhone("Samsung", "Galaxy S21", 799.99);
+        
+        mobile1.displayDetails();
+        mobile2.displayDetails();
+    }
+}
