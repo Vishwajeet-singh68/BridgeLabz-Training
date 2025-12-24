15c618bd1ea993898a13cfca98ac4eeff3b7317c
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2025-12-24 16:07:55 +0530

    [Vishwajeet Singh] Add . Book library system code added

---
+++ OOPs/gcr-codebase/constructors/BookLibrarySystem/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        EBook eBook = new EBook("0987654321", "Advanced Java", "Jane Smith");
+
+        System.out.println();
+        eBook.displayDetails();
+    }
+}
