7ba2c8de8a4952d43a2bfa44703feb6a593c10ff
Vishwajeet-singh68 <vishwajeet04022006@gmail.com>
2026-01-01 00:55:36 +0530

    [Vishwajeet Singh] Refactor . Emlpoyee wage code with some better readability by keeping constants in different clss

---
+++ oops-practice/scenario-based/Employee.java
@@ -0,0 +1,31 @@
+package EmployeeWageComputation;
+
+public class Employee {
+    private int totalWorkingHours;
+    private int totalWorkingDays;
+    private int totalWage;
+
+    public void addWorkingHour(int hours){
+        totalWorkingHours+=hours;
+    }
+
+    public void addWorkingDay(){
+        totalWorkingDays++;
+    }
+
+    public void addWage(int wage){
+        totalWage+=wage;
+    }
+
+    public int getTotalWorkingHours() {
+        return totalWorkingHours;
+    }
+
+    public int getTotalWorkingDays() {
+        return totalWorkingDays;
+    }
+
+    public int getTotalWage() {
+        return totalWage;
+    }
+}
---
+++ oops-practice/scenario-based/WageCalculation.java
@@ -0,0 +1,8 @@
+package EmployeeWageComputation;
+
+public class WageCalculation {
+
+    public int calculateWage(int workingHours){
+        return workingHours*CompanyPolicy.WAGE_PER_HOUR;
+    }
+}
