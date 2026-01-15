a486512a1d31ce4104574f532c372c6ebf6c2d6c
Vishwajeet-singh68 <122093754+Vishwajeet-singh68@users.noreply.github.com>
2026-01-15 15:27:04 +0530

    [Vishwajeet Singh] Refactor . removed some ambiguity
    Added default bad words to the censoring function.

--- collection-practice/gcr-codebase/regex/CensorBadWords.java
+++ collection-practice/gcr-codebase/regex/CensorBadWords.java
@@ -2,6 +2,9 @@ import java.util.*;
 
 public class CensorBadWords {
     public static String censorBadWords(String text, List<String> badWords) {
+        List<String> badWords=new ArrayList<>();
+        badWords.add("damn");
+        badWords.add("stupid");
         for (String word : badWords) {
             text = text.replaceAll("(?i)\\b" + word + "\\b", "****");
         }
